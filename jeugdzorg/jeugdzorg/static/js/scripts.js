e187362c4adb977cd17669a5914e7691ac46a0da
fixxxdev <fixxxdev@tick.nl>
2018-02-08 12:21:38 +0100

    Add script to base template.

---
+++ jeugdzorg/jeugdzorg/static/js/scripts.js
@@ -0,0 +1,53 @@
+!function(w, d){
+  
+  var handlers = {
+    'list-add': function(){
+      this.list.appendChild(this.template.cloneNode(true));
+    }
+    ,
+    'list-remove': function(){
+      this.parentNode.parentNode.removeChild(this.parentNode);
+    }
+  };
+  
+  var decorators = {
+    
+    'list': function(){
+      
+      var remove = document.createElement('button');
+      remove.dataset.handler = 'list-remove';
+      remove.type = 'button';
+      remove.innerHTML = '-';
+
+      for (var i=0; i<this.children.length; i++) {
+        this.children[i].appendChild(remove.cloneNode(true));
+      }
+      
+      var add = document.createElement('button');
+      add.dataset.handler = 'list-add';
+      add.type = 'button';
+      add.innerHTML = '+';
+
+      add.list = this;
+
+      add.template = this.firstElementChild.cloneNode(true);
+      add.template.querySelector('textarea').value = '';
+
+      this.parentNode.appendChild(add);
+    }
+
+  };
+  
+  var helpers = {
+
+  };
+  
+  d.addEventListener('click',function(t){var k,e,a=t&&t.target;if(a=_closest(a,'[data-handler]')){var r=a.getAttribute('data-handler').split(/\s+/);if('A'==a.tagName&&(t.metaKey||t.shiftKey||t.ctrlKey||t.altKey))return;for(e=0;e<r.length;e++){k=r[e].split(/[\(\)]/);handlers[k[0]]&&handlers[k[0]].call(a,t,k[1])}}});
+
+  var _decorate = function(){
+    var k,i,j,decoratorString,el,els=d.querySelectorAll('[data-decorator]');for(i=0;i<els.length;i++){for(decoratorString=(el=els[i]).getAttribute('data-decorator').split(/\s+/),j=0;j<decoratorString.length;j++){k=decoratorString[j].split(/[\(\)]/);decorators[k[0]]&&decorators[k[0]].call(el,k[1]);el.removeAttribute('data-decorator')}}    
+  }; _decorate();
+
+  var _closest=function(e,t){var ms='MatchesSelector',c;['matches','webkit'+ms,'moz'+ms,'ms'+ms,'o'+ms].some(function(e){return'function'==typeof document.body[e]&&(c=e,!0)});var r=e;try{for(;e;){if(r&&r[c](t))return r;e=r=e.parentElement}}catch(e){}return null};
+
+}(window, document.documentElement);
