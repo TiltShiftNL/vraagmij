a1bd794027325e6d2a6e344ef176e11bfbfd8a96
Maurice Guikema <>
2018-02-15 10:32:26 +0100

    jenkinsfile test

--- docker-local-entrypoint.sh
+++ docker-local-entrypoint.sh
@@ -5,5 +5,12 @@ python manage.py migrate
 
 python manage.py createsuperuser_pw  --username ${ADMIN_USERNAME} --password ${ADMIN_PASSWORD} --noinput --email 'admin@host.com'
 
-# tail -f /etc/hosts
-python ./manage.py runserver 0.0.0.0:8000 --settings jeugdzorg.settings.develop
+
+if [ "${RUNSERVER}" == "no" ];
+then
+    echo "Don't start server"
+    tail -f /etc/hosts
+else
+    echo "Start server"
+    python ./manage.py runserver 0.0.0.0:8000 --settings jeugdzorg.settings.develop
+fi
