c5f6396840b3d69063dbb98a664e3958d395b917
fixxxdev <fixxxdev@tick.nl>
2018-02-22 11:14:08 +0100

    Progress WIP

--- jeugdzorg/jeugdzorg/static/js/scripts.js
+++ jeugdzorg/jeugdzorg/static/js/scripts.js
@@ -49,21 +49,33 @@
       var el = this;
       var id = el.dataset.regelingId;
       var items = el.children;
+      var hashIcons = {
+        yes: '■',
+        no: '□',
+        maybe: '▣'
+      };
 
-      // var progress = document.createElement('svg');
-      // progress.classList.add('progress');
-      // progress.width = 88;
-      // progress.height = 88;
-      // progress.viewBox = '0 0 88 88';
-      // progress.innerHTML = '';
-      // progress.innerHTML += '<circle class="progress__meter" cx="44" cy="44" r="41" stroke-width="6" />';
-      // progress.innerHTML += '<circle class="progress__value" cx="44" cy="44" r="41" stroke-width="6" />';
-      
-      var progress = document.createElement('span');
-      
+      var hashIcons = {
+        yes: '+',
+        no: '-',
+        maybe: '_'
+      };
+
+      var progress = document.createElement('svg');
       progress.classList.add('regeling-progress');
+      progress.width = 88;
+      progress.height = 88;
+      progress.viewBox = '0 0 88 88';
+      progress.innerHTML = '';
+      progress.innerHTML += '<circle class="initial" cx="44" cy="44" r="41" stroke-width="6" />';
+      progress.innerHTML += '<circle class="yes" cx="44" cy="44" r="41" stroke-width="6" />';
+      progress.innerHTML += '<circle class="no" cx="44" cy="44" r="41" stroke-width="6" />';
       
-      progress.innerHTML = '<span class="yes"></span><span class="no"></span><span class="result"></span>';
+      // var progress = document.createElement('span');
+      
+      // progress.classList.add('regeling-progress');
+      
+      // progress.innerHTML = '<span class="yes"></span><span class="no"></span><span class="result"></span>';
       el.parentNode.appendChild(progress);
       var 
         resultEl = progress.querySelector('.result'),
@@ -77,23 +89,36 @@
           maybe = 0,
           yes = 0,
           no = 0;
+          resultHash = [];
+
+        resultHash.push('[');
           
         for (var i=0; i<items.length; i++) {
-          if (items[i].querySelectorAll('[value="yes"]:checked').length) yes++;
-          else if (items[i].querySelectorAll('[value="no"]:checked').length) no++;
-          else if (items[i].querySelectorAll('[value="maybe"]:checked').length) maybe++;
+          if (items[i].querySelectorAll('[value="yes"]:checked').length) {
+            resultHash.push(hashIcons.yes);
+            yes++;
+          }
+          else if (items[i].querySelectorAll('[value="no"]:checked').length) {
+            resultHash.push(hashIcons.no);
+            no++;
+          }
+          else if (items[i].querySelectorAll('[value="maybe"]:checked').length) {
+            resultHash.push(hashIcons.maybe);
+          }
         }
+        resultHash.push(']');
+        document.location.hash = resultHash.join('');
         
-        resultEl.innerHTML = Math.round(yes / total * 100) + '<em>%</em>';
+        // resultEl.innerHTML = Math.round(yes / total * 100) + '<em>%</em>';
         
         var 
           yesResult = Math.round(yes / total * 100),
           noResult = Math.round(no / total * 100);
         
-        yesEl.style.width = yesResult + '%';
-        yesEl.style.height = yesResult + '%';
-        noEl.style.width = (yesResult + noResult) + '%';
-        noEl.style.height = (yesResult + noResult) + '%';
+        // yesEl.style.width = yesResult + '%';
+        // yesEl.style.height = yesResult + '%';
+        // noEl.style.width = (yesResult + noResult) + '%';
+        // noEl.style.height = (yesResult + noResult) + '%';
         
         progress.classList[(yes + no == total) ? 'add' : 'remove']('complete');
         progress.classList[(yes + no > 0) ? 'add' : 'remove']('started');
@@ -119,12 +144,32 @@
         yesno.innerHTML = '';
         
         yesno.innerHTML += '<input id="voorwaarde-' + id + '-' + items[i].dataset.voorwaardeId + '-yes" name="voorwaarde-' + id + '-' + items[i].dataset.voorwaardeId + '" type="radio" value="yes">';
-        yesno.innerHTML += '<input id="voorwaarde-' + id + '-' + items[i].dataset.voorwaardeId + '-no" name="voorwaarde-' + id + '-' + items[i].dataset.voorwaardeId + '" type="radio" value="no">';
-        yesno.innerHTML += '<input id="voorwaarde-' + id + '-' + items[i].dataset.voorwaardeId + '-initial" name="voorwaarde-' + id + '-' + items[i].dataset.voorwaardeId + '" type="radio" value="maybe" checked>';
         yesno.innerHTML += '<label for="voorwaarde-' + id + '-' + items[i].dataset.voorwaardeId + '-yes">Ja</label>';
+
+        yesno.innerHTML += '<input id="voorwaarde-' + id + '-' + items[i].dataset.voorwaardeId + '-no" name="voorwaarde-' + id + '-' + items[i].dataset.voorwaardeId + '" type="radio" value="no">';
         yesno.innerHTML += '<label for="voorwaarde-' + id + '-' + items[i].dataset.voorwaardeId + '-no">Nee</label>';
+
+        yesno.innerHTML += '<input id="voorwaarde-' + id + '-' + items[i].dataset.voorwaardeId + '-initial" name="voorwaarde-' + id + '-' + items[i].dataset.voorwaardeId + '" type="radio" value="maybe" checked>';
         yesno.innerHTML += '<label for="voorwaarde-' + id + '-' + items[i].dataset.voorwaardeId + '-initial">Misschien</label>';
         items[i].appendChild(yesno);
+      }
+      
+      // setting initial state based on hash
+      
+      if (w.location.hash) {
+        var initial = w.location.hash.match(/\[([\+-_]*)\]/);
+        if (initial && initial[1]) {
+          var state = initial[1].split('');
+          
+          if (items.length == state.length) {
+          
+            for (var i=0; i<state.length; i++) {
+              if (state[i] == hashIcons.yes) items[i].querySelectorAll('[value="yes"]')[0].checked = true;
+              else if (state[i] == hashIcons.no) items[i].querySelectorAll('[value="no"]')[0].checked = true;
+            }
+            
+          }
+        }
       }
       
       el.addEventListener('change', _change); _change();
