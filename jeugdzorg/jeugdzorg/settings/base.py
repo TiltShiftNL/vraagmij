3ceaae741d0988f9b6b41b05caae662169eb3b46
Maurice Guikema <>
2018-02-24 22:18:23 +0100

    db host to noset

--- jeugdzorg/jeugdzorg/settings/base.py
+++ jeugdzorg/jeugdzorg/settings/base.py
@@ -159,7 +159,7 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
-PYTHONHASHSEED = 0
+CRONTAB_EXECUTABLE = '/usr/bin/crontab -e'
 
 CRONJOBS = [
     ('*/10 * * * *', 'jeugdzorg.cron.update_regeling_bron_job', '>> /var/log/scheduled_job.log 2>&1')
--- jeugdzorg/jeugdzorg/settings/production.py
+++ jeugdzorg/jeugdzorg/settings/production.py
@@ -4,4 +4,6 @@ from .base import *
 #     ('*/5 * * * *', 'jeugdzorg.cron.update_regeling_bron_job')
 # ]
 
+CRONTAB_DJANGO_SETTINGS_MODULE = ''
+
 DEBUG = False
