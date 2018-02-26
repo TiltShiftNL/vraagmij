530bd338150a2075cfe7c9b4dac5ac96f25049ff
Maurice Guikema <>
2018-02-26 23:36:20 +0100

    crontab variables created

--- jeugdzorg/jeugdzorg/management/commands/update_regeling_bron.py
+++ jeugdzorg/jeugdzorg/management/commands/update_regeling_bron.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.management.commands import createsuperuser
 from django.core.management import CommandError
-from jeugdzorg.cron import print_variables
+from jeugdzorg.cron import print_variables, update_regeling_bron_job
 
 
 class Command(createsuperuser.Command):
     help = 'Crate a superuser, and allow password to be provided'
 
     def handle(self, *args, **options):
-        print_variables()
+        update_regeling_bron_job()
