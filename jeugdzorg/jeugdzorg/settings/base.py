e417cf6e239030aee1e345908d78e5123f32b537
Maurice Guikema <>
2018-03-06 12:53:15 +0100

    enable admin

--- jeugdzorg/jeugdzorg/settings/base.py
+++ jeugdzorg/jeugdzorg/settings/base.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'storages',
     'import_export',
 
-    #'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
