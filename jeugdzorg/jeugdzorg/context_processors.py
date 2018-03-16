bd547ae93adfbfb06bf706290bc75cfc3a7e8a43
fixxxdev <fixxxdev@tick.nl>
2018-03-16 16:49:25 +0100

    Add totals for usage in templates.

--- jeugdzorg/jeugdzorg/context_processors.py
+++ jeugdzorg/jeugdzorg/context_processors.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
+from jeugdzorg.models import Regeling
+from jeugdzorg.models import Profiel
+from jeugdzorg.models import Thema
 
 
 def app_settings(request):
@@ -8,4 +11,7 @@ def app_settings(request):
         'ENV': settings.ENV,
         'SOURCE_COMMIT': settings.SOURCE_COMMIT,
         'UWSGI_TEST': request.environ.get('UWSGI_TEST'),
+        'REGELING_COUNT': Regeling.objects.all().count(),
+        'PROFIEL_COUNT': Profiel.is_zichtbaar.all().count(),
+        'THEMA_COUNT': Thema.objects.all().count()
     }
