c6a6510fa4ae7d6176b5b51fd82b37920cedad98
Maurice Guikema <>
2018-03-21 19:10:26 +0100

    send update mail

--- jeugdzorg/jeugdzorg/management/commands/send_update_mail.py
+++ jeugdzorg/jeugdzorg/management/commands/send_update_mail.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.management.commands import createsuperuser
 from django.core.management.base import BaseCommand, CommandError
 from django.core.management import CommandError
+from django.contrib.sites.models import Site
 
 
 class Command(BaseCommand):
@@ -8,3 +9,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print('Send update mail')
+        site = Site.objects.get_current()
+        print(site)
