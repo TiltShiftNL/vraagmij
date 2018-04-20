f08e82622a9c1b33a55160f0d7bef0cf95e09d5a
Maurice Guikema <>
2018-04-20 09:35:26 +0200

    timezones active for all scripts

--- jeugdzorg/mail_account_active_check.sh
+++ jeugdzorg/mail_account_active_check.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+export TZ="Europe/Amsterdam"
+export CRON_TZ="Europe/Amsterdam"
 /usr/local/bin/python /opt/app/manage.py mail_account_active_check
--- jeugdzorg/send_update_mail.sh
+++ jeugdzorg/send_update_mail.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+export TZ="Europe/Amsterdam"
+export CRON_TZ="Europe/Amsterdam"
 /usr/local/bin/python /opt/app/manage.py send_update_mail
